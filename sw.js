a71b1822341af1fd974cc4a1cdc67366130c0a72
nongmee <nongmee1225@gmail.com>
2026-08-23 19:42:37 +0700

    Redesign PWA launcher experience

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kth-sepsis-pwa-v2';
+const CACHE_NAME = 'kth-sepsis-pwa-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)));
